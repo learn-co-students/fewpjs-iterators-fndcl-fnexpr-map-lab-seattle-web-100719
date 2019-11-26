a1b658d3f1f90ddf55b1ef5da0038e3f63f4dd18
alisabaj <alisabaj@gmail.com>
2019-11-26 15:24:08 -0800

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    let capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.substring(1)
+    })
+    return capitalizedWords.join(" ")
+  })
 }
